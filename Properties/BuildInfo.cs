fbe7ee9b13fe0119801d0ad6f7d741719d887e63
Fuar11 <mcaa6556@gmail.com>
2026-01-06 16:30:41 -0500

    Update to 2.51

--- Properties/BuildInfo.cs
+++ Properties/BuildInfo.cs
@@ -7,9 +7,9 @@ internal static class BuildInfo
     public const string Description = null!;
     public const string Author = "Fuar, Deadman";
     public const string Copyright = "Copyright © 2024";
-    public const string Version = "1.0.2";
+    public const string Version = "1.0.3";
     public const string AssemblyVersion = $"{Version}.0";
-    public const string MelonLoaderVersion = "0.6.6";
+    public const string MelonLoaderVersion = "0.7.2";
     public const string GUIName = "Affliction Component";
 
     // Optional
